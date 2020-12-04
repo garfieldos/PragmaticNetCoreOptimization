66b9e69c857838764cc021926eccfd1b705e711a
szymon@gaertig.com.pl <szymon@gaertig.com.pl>
2020-12-04 14:12:01 +0100

    init

---
+++ Benchmarks/ArrayPool/RentReturn_vs_allocate.cs
@@ -0,0 +1,35 @@
+﻿using System.Buffers;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Jobs;
+
+namespace Benchmarks.ArrayPool
+{
+    [MemoryDiagnoser]
+    [SimpleJob(RuntimeMoniker.NetCoreApp31)]
+    public class RentReturn_vs_allocate
+    {
+        [Params(100,1_000,50_000,84_000,85_000, 100_000, 200_000)]
+        public int ArrayLength { get; set; }
+
+        private ArrayPool<byte> _pool;
+
+        [GlobalSetup]
+        public void GlobalSetup()
+        {
+            _pool = ArrayPool<byte>.Shared;
+        }
+
+        [Benchmark]
+        public void RendAndReturn()
+        {
+            var array = _pool.Rent(ArrayLength);
+            _pool.Return(array);
+        }
+
+        [Benchmark]
+        public void Allocate()
+        {
+            var array = new byte[ArrayLength];
+        }
+    }
+}
---
+++ Benchmarks/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using BenchmarkDotNet.Running;
+
+namespace Benchmarks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if ( args.Length != 1 || string.IsNullOrWhiteSpace(args[0]))
+            {
+                Console.WriteLine("Please provide benchmark name");
+                return;
+            }
+                
+            var benchMarkName = args[0];
+
+            var benchmarkType = Assembly.GetAssembly(typeof(Program))
+                .GetTypes().FirstOrDefault(x => x.Name == benchMarkName);
+            
+            if (benchmarkType == null)
+            {
+                Console.Write($"Could not find benchmark: {benchmarkType}");
+                return;
+            }
+            
+            BenchmarkRunner.Run(benchmarkType);
+        }
+    }
+}
